379760cd8486073648ea03476a7d717516756f70
Shreyash Purankar <purankar.shreyash54321@gmail.com>
2025-07-15 23:02:43 +0530

    Add routing in ThoughtCove

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,69 @@ import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
 import store from './store/store.js'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { ProtectedLayout } from './components/index.js'
+
+import { AddPost, AllPosts, EditPost, Home, Login, Post, Signup } from "./pages"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/login",
+        element: (
+          <ProtectedLayout authentication={false}>
+            <Login />
+          </ProtectedLayout>
+        )
+      },
+      {
+        path: "/signup",
+        element: (
+          <ProtectedLayout authentication={false}>
+            <Signup />
+          </ProtectedLayout>
+        )
+      },
+      {
+        path: "/all-posts",
+        element: (
+          <ProtectedLayout authentication={true}>
+            <AllPosts />
+          </ProtectedLayout>
+        )
+      },
+      {
+        path: "/add-post",
+        element: (
+          <ProtectedLayout authentication={true}>
+            <AddPost />
+          </ProtectedLayout>
+        )
+      },
+      {
+        path: "/edit-post/:slug",
+        element: (
+          <ProtectedLayout authentication={true}>
+            <EditPost />
+          </ProtectedLayout>
+        )
+      },
+      {
+        path: "/post/:slug",
+        element: <post />
+      }
+    ]
+  }
+])
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-    <App />
+      <RouterProvider router={router} />
     </Provider>
   </StrictMode>,
 )
